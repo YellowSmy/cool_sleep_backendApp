16a8bfc43eecf19c690ea24bdd398f38ecf8df21
BluesSmy <fprh2005@naver.com>
2021-08-21 12:47:43 +0900

    UI update, apiView update

--- phrase/drf_api/Apiviews.py
+++ phrase/drf_api/Apiviews.py
@@ -4,15 +4,19 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .serializers import PhraseSerializer, KnowledgeSerializer
 
+import random
+
 ##### API VIEW #####
 @api_view(['GET'])
 def randomPhrase(req):
-    phrase = Phrase.objects.order_by('?').first()
-    serializer = PhraseSerializer(phrase)
+    phrases = Phrase.objects.all()
+    randomphrases = random.sample(list(phrases), 4)
+    serializer = PhraseSerializer(randomphrases, many=True)
     return Response(serializer.data)
 
 @api_view(['GET'])
 def randomKnowledge(req):
-    knowledge = SleepKnowledge.objects.order_by('?').first()
-    serializer = KnowledgeSerializer(knowledge)
+    knowledges = SleepKnowledge.objects.all()
+    randomknowledges = random.sample(list(knowledges), 4)
+    serializer = KnowledgeSerializer(randomknowledges, many=True)
     return Response(serializer.data)
