51e21332679eacd80f78ae53cab801ea4207970f
BluesSmy <fprh2005@naver.com>
2021-10-10 15:56:26 +0900

    security update

--- phrase_api/settings.py
+++ phrase_api/settings.py
@@ -17,11 +17,12 @@ import django_heroku
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'bt#uxu-dd%()qd9^95qh0-@#kz)a@yfx*i_#gq&6a^+^@9dc8$')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
