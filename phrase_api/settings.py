4ccf713a7f442d47980065febc484baecdfe170b
BluesSmy <fprh2005@naver.com>
2021-10-10 16:06:05 +0900

    NO

--- phrase_api/settings.py
+++ phrase_api/settings.py
@@ -23,6 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'bt#uxu-dd%()qd9^95qh0-@#kz)a@yfx*i_#gq&6a^+^@9dc8$')
+#NO
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
